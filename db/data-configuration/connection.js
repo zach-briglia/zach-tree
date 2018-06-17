2cdca298dfad553742c420302c0e74fdf4a1011e
Zach Briglia <zbrigli@gmail.com>
2018-06-17 19:42:55 -0400

    Adding connection disconnect.

--- db/data-configuration/connection.js
+++ db/data-configuration/connection.js
@@ -3,26 +3,11 @@ const mySQL = require('mysql');
 
 var conn;
 
-function handleDisconnect() {
-    conn = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL);
-    conn.connect(function(err) {
-        if (err){
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000);
-        }
-        console.log("MYSQL connected")
-    });
+conn = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL);
 
-    conn.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-            handleDisconnect();                         
-        } else {                                      
-            throw err;
-        }
-      });
-}
-
-handleDisconnect();
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("MYSQL connected")
+});
 
 module.exports = conn;
