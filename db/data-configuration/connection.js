563ab237197938af9f6490df4a56c4d55be1366e
Zach Briglia <zbrigli@gmail.com>
2018-06-17 18:19:07 -0400

    Commiting .env file to heroku.

--- db/data-configuration/connection.js
+++ db/data-configuration/connection.js
@@ -2,7 +2,7 @@ const mySQL = require('mysql');
 
 var conn;
 
-conn = mySQL.createConnection(process.env.DATABASE_URL);
+conn = mySQL.createConnection('mysql://b97ec1b95d497a:143f2ac0@us-cdbr-iron-east-04.cleardb.net/heroku_4a9ac6654cfc0f6?reconnect=true');
 
 conn.connect(function(err) {
     if (err) throw err;
