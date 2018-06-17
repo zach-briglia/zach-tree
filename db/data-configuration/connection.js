755d5cfa7861960dc9d5683244e106ebd6a8d6a3
Zach Briglia <zbrigli@gmail.com>
2018-06-17 16:50:06 -0400

    Adding db config.

--- db/data-configuration/connection.js
+++ db/data-configuration/connection.js
@@ -3,10 +3,10 @@ const mySQL = require('mysql');
 var conn;
 
 conn = mySQL.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "tree_node_view_db"
+    host: "us-cdbr-iron-east-04.cleardb.net",
+    user: "b97ec1b95d497a",
+    password: "143f2ac0",
+    database: "heroku_4a9ac6654cfc0f6"
 });
 
 conn.connect(function(err) {
