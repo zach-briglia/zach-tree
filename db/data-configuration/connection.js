fa92fb0b8e20bec8a91834b79d8b946ac6d6140d
Zach Briglia <zbrigli@gmail.com>
2018-06-17 19:16:08 -0400

    Working on server timeout.

--- db/data-configuration/connection.js
+++ db/data-configuration/connection.js
@@ -3,11 +3,26 @@ const mySQL = require('mysql');
 
 var conn;
 
-conn = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL);
+function handleDisconnect() {
+    conn = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL);
+    conn.connect(function(err) {
+        if (err){
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+        console.log("MYSQL connected")
+    });
 
-conn.connect(function(err) {
-    if (err) throw err;
-    console.log("MYSQL connected")
-});
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+            handleDisconnect();                         
+        } else {                                      
+            throw err;
+        }
+      });
+}
+
+handleDisconnect();
 
 module.exports = conn;
