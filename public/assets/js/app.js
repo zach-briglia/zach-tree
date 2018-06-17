76882b6f0e62a64a697c000a575682a51da980a8
Zach Briglia <zbrigli@gmail.com>
2018-06-17 14:11:42 -0400

    Removing some unnecessary code. Fixing alert dismissal issue. Adding new modals to edit factory values and to support adding a parent.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,25 +9,40 @@ socket.on('getParents',function(result) {
 
     for(var i = 0; i < result.length; i++){
 
-        var $parent = `<button id="button${result[i].parent_id}" data-id="${result[i].parent_id}" data-children="${result[i].children}" data-range-start="${result[i].parent_lower_bound}" data-range-end="${result[i].parent_upper_bound}" type="button" class="btn btn-parent parentBG d-block mb-2" data-toggle="modal" data-target="#rootModal${result[i].parent_id}"><strong>${result[i].parent_name}</strong></button>
+        var $parent = `<div class="btn-group", role="group"><button id="button${result[i].parent_id}" data-id="${result[i].parent_id}" data-children="${result[i].children}" data-range-start="${result[i].parent_lower_bound}" data-range-end="${result[i].parent_upper_bound}" type="button" class="btn btn-parent parentBG d-block mb-2" data-toggle="modal" data-target="#editParentNameModal${result[i].parent_id}"><strong>${result[i].parent_name}</strong></button><button id="button${result[i].parent_id}" data-id="${result[i].parent_id}" data-children="${result[i].children}" data-range-start="${result[i].parent_lower_bound}" data-range-end="${result[i].parent_upper_bound}" type="button" class="btn btn-default d-block mb-2" data-toggle="modal" data-target="#editChildrenModal${result[i].parent_id}"><strong>&#x2261;</strong></button></div>
 
-        <div id="rootModal${result[i].parent_id}" class="modal fade" role="dialog">
+        <div id="editParentNameModal${result[i].parent_id}" class="modal fade" role="dialog">
             <div class="modal-dialog">
           
               <!-- Modal content-->
               <div class="modal-content">
                 <div class="modal-header">
-                  <h4 class="modal-title">${result[i].parent_name} details</h4>
+                  <h4 class="modal-title">${result[i].parent_name} name</h4>
                   <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
                     <div class="row"><input id="editParentName${result[i].parent_id}" required value="${result[i].parent_name}" class="form-control"/></div>
-                    <div class="row"><div class="col-sm-4">
-                        <button id="editParentNameButton" class="btn btn-success my-1 mr-1" onclick="editParent(${result[i].parent_id})" data-dismiss="modal">save name</i></button>
-                    </div>
-                    <div class="col-sm-4">
-                        <button onclick="deleteItem(${result[i].parent_id})" class="btn btn-warning node-delete my-1 mr-1" data-dismiss="modal">delete parent</button>
-                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button id="editParentNameButton" class="btn btn-success my-1 mr-1" onclick="editParent(${result[i].parent_id})" data-dismiss="modal">save name</i></button>
+                    <button onclick="deleteItem(${result[i].parent_id})" class="btn btn-warning node-delete my-1 mr-1" data-dismiss="modal">delete parent</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+          
+            </div>
+          </div>
+
+          <div id="editChildrenModal${result[i].parent_id}" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+          
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title">${result[i].parent_name} factories</h4>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
                     <div class="container mb-2"><div class="row"><input id="numChildren${result[i].parent_id}" required value="${result[i].children}" class="form-control input-md mb-3" type="number" min="0" max="15" placeholder="Number of Leaves"><div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text parentBG">Range</span></div><input required id="rangeLowerBound${result[i].parent_id}" type="number" class="form-control" placeholder="Min" value="${result[i].parent_lower_bound}"><input required id="rangeUpperBound${result[i].parent_id}" type="number" class="form-control" placeholder="Max" value="${result[i].parent_upper_bound}"></div>
                 </div>
                 <div class="modal-footer">
@@ -38,6 +53,7 @@ socket.on('getParents',function(result) {
           
             </div>
           </div>`
+          
         $($parentRoot).append($parent);
         console.log(result[i])
         if (parseInt(result[i].children) !== null && parseInt(result[i].children) > 0) {
@@ -59,7 +75,7 @@ socket.on('getParents',function(result) {
     $parents.append($parentRoot);
 });
 
-$("#parentSubmit").click(function(event){
+$("#addParentModal").on('click', "#parentSubmit", function(event){
     event.preventDefault();
     var $inputNode = $("#inputRootItem");
     var $newName = $inputNode.val().trim();
@@ -86,13 +102,17 @@ function deleteItem(parentID){
 };
 
 function editParent(parentID) {
-    var $editedNameNode = document.getElementById("editParentName");
     var $editedName = document.getElementById("editParentName" + parentID).value;
+    var $emptyParent = $("#emptyParent");
+    var $invalidParent = $("#invalidParent");
     var pattern = new RegExp(/^\w+$/);
     isValidInput = pattern.test($editedName);
     if (!isValidInput) {
-        $($editedNameNode).css("borderColor", "yellow");
-    } else {
+        $invalidParent.show();
+    } else if ($editedName === ""){
+        $emptyParent.show();
+    }
+    else {
         //console.log($editedName);
         socket.emit("updateParentName", $editedName, parentID);
     }
@@ -108,3 +128,7 @@ function editFactories(parentID) {
     // console.log($numChildren)
     socket.emit('createChildren', parentID, $childrenLowerBound, $childrenUpperBound, $numChildren)
 }
+
+$("[data-hide]").on("click", function(){
+    $(this).closest("." + $(this).attr("data-hide")).hide();
+});
