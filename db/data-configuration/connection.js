bffe55030f62e9b13ddf7875edacbe31b66421d8
Zach Briglia <zbrigli@gmail.com>
2018-06-17 18:31:39 -0400

    Trying environment file one more time...

--- db/data-configuration/connection.js
+++ db/data-configuration/connection.js
@@ -3,7 +3,7 @@ const mySQL = require('mysql');
 
 var conn;
 
-conn = mySQL.createConnection(process.env.DATABASE_URL);
+conn = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL);
 
 conn.connect(function(err) {
     if (err) throw err;
