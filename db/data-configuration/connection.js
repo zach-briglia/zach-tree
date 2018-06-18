c26d8df54fd967d5306d50162b66fbeafd0bac46
Zach Briglia <zbrigli@gmail.com>
2018-06-17 20:44:04 -0400

    Working on server timeout.

--- db/data-configuration/connection.js
+++ db/data-configuration/connection.js
@@ -1,31 +1,16 @@
 require('dotenv').config();
 const mySQL = require('mysql');
 
-var connection;
-connection = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL); 
-handleDisconnect(connection);
+var conn;
 
-function handleDisconnect(client) {
-    connection = mySQL.createConnection(process.env.CLEARDB_DATABASE_URL); 
-                                                    
+conn = mySQL.createConnection(process.env.JAWSDB_URL);
 
-    connection.connect(function(err) {              
-        if(err) {                                     
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect(), 2000); 
-        }                                     
-    });                                     
-                                            
-    connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-            handleDisconnect(connection);                         
-        } else {                                      
-            throw err;
-            return;                                  
-        }
-    });
-}
-handleDisconnect(connection)
+conn.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + conn.threadId);
+});
 
-module.exports = connection;
+module.exports = conn;
